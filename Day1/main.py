0b9a4edb27584237c6767328dd0923daab6f0916
JonesKwameOsei <oseikwamejones@gmail.com>
2024-05-28 02:12:01 +0100

    adding day1 files

---
+++ Day1/main.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+
+print(len(input()))
+
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+c = a
+a = b
+b = c
+print("a: " + a)
+print("b: " + b)
